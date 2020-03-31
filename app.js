49e55afa221ab79c568a7886319cafb43ded1a3c
sandeep501 <sandeepanmi1997@gmail.com>
2020-03-31 15:59:51 +0530

    hello

---
+++ app.js
@@ -0,0 +1,26 @@
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
+
+
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=23c7abd85fd57c0868a9f281f80e8911')
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  var currentTempValue=Math.round(parseFloat(tempValue)-273.15);
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Desc - "+descValue;
+  temp.innerHTML = "Temp - "+currentTempValue +" &deg; C";
+  input.value ="";
+
+})
+
+.catch(err => alert("Wrong city name!"));
+})
